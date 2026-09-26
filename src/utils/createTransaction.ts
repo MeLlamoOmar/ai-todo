3c3b4c56a59c204cbab20d8ab767a86bc02610fa
Omar Bencosme Tio <obencosmetio@gmail.com>
2026-09-26 11:56:48 -0400

    Last clean up from un-used variable

--- src/utils/createTransaction.ts
+++ src/utils/createTransaction.ts
@@ -1,14 +1,13 @@
-import type { Transaction, TransactionFormValues } from '../types/transaction'
-import { validateTransactionValues } from './validateTransactionValues'
+import type { Transaction, TransactionFormValues } from '../types/transaction';
+import { validateTransactionValues } from './validateTransactionValues';
 
 export function createTransaction(values: TransactionFormValues): Transaction {
-  const transactionValues = validateTransactionValues(values)
-  const timestamp = new Date().toISOString()
+  const transactionValues = validateTransactionValues(values);
+  const timestamp = new Date().toISOString();
 
   return {
     id: crypto.randomUUID(),
     ...transactionValues,
-    createdAt: timestamp,
     updatedAt: timestamp,
-  }
+  };
 }
